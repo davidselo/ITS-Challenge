01dd0364281de51b16488cdb65e769d5827bb335
David Villalba Flores <david.selo@gmail.com>
2023-02-12 18:36:35 +0000

    Adding prettierrc file and code styling changes

--- src/models/order.ts
+++ src/models/order.ts
@@ -1,39 +1,36 @@
-import { SqLiteClient } from "../client/sqLiteClient";
-import { getOrdersWithTotals, getOrdersWithItems, findByOrderId } from "../client/SqlQueries/orderQueries";
+import { SqLiteClient } from '../client/sqLiteClient';
+import { getOrdersWithTotals, getOrdersWithItems, findByOrderId } from '../client/SqlQueries/orderQueries';
 export class Order {
+  dbHandler;
 
-    dbHandler;
-    
-    constructor(dbHandler: SqLiteClient){
-        this.dbHandler = dbHandler;
-    }
+  constructor(dbHandler: SqLiteClient) {
+    this.dbHandler = dbHandler;
+  }
 
-    getOrdersWithTotals(){
-        this.dbHandler.db.each(getOrdersWithTotals, (error:any, row: any) => {
-            if (error) {
-                return console.log(error.message);
-            }
-            console.log(row);
-        })
-        
-    }
+  getOrdersWithTotals() {
+    this.dbHandler.db.each(getOrdersWithTotals, (error: any, row: any) => {
+      if (error) {
+        return console.log(error.message);
+      }
+      console.log(row);
+    });
+  }
 
-    getOrdersWithItems(){
-        this.dbHandler.db.each(getOrdersWithItems,(error:any, row: any) => {
-            if (error) {
-                return console.log(error.message);
-            }
-            console.log(row);
-        })
-    }
+  getOrdersWithItems() {
+    this.dbHandler.db.each(getOrdersWithItems, (error: any, row: any) => {
+      if (error) {
+        return console.log(error.message);
+      }
+      console.log(row);
+    });
+  }
 
-    findByOrderId(orderId: string){
-        this.dbHandler.db.get(findByOrderId, orderId , (error:any, row: any) => {
-            if (error) {
-                return console.log(error.message);
-            }
-            console.log(row);
-        })
-    }
-
+  findByOrderId(orderId: string) {
+    this.dbHandler.db.get(findByOrderId, orderId, (error: any, row: any) => {
+      if (error) {
+        return console.log(error.message);
+      }
+      console.log(row);
+    });
+  }
 }
